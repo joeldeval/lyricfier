d4fb7a54be77b6031536c5f0752d93f9356b9ab0
Joel Valdivia <jooelvaldivi@gmail.com>
2017-06-12 17:09:38 -0500

    replace async.detectSeries for async.eachSeries

--- render/Searcher.ts
+++ render/Searcher.ts
@@ -1,9 +1,10 @@
 import {SearchLyrics} from './plugins/SearchLyrics';
 import {SearchWikia} from "./plugins/SearchWikia";
+
 import {Genius} from "./plugins/Genius";
 import {NormalizeTitles} from "./NormalizeTitles";
 const async = require('async');
-const plugins = [SearchWikia, Genius];
+const plugins = [Genius, SearchWikia];
 const request = require('request').defaults({timeout: 5000});
 
 interface Result {
@@ -22,6 +23,7 @@ export class Searcher {
     }
 
     loadPlugins() {
+        
         this.plugins = plugins.map((Plugin) => {
             return new Plugin(request);
         });
@@ -29,16 +31,23 @@ export class Searcher {
 
     search(title:string, artist:string, cb : (error : any, result : Result) => void) {
         const from = { lyric: null, sourceName: '', sourceUrl: ''};
+        
         const normalizedTitle = this.normalizer.normalize(title);
         // run plugins on series
         // if some returns success getting a lyric
         // stop and save the lyric result
-        async.detectSeries(this.plugins, (plugin : SearchLyrics, callback) => {
+        console.log(this.plugins);
+        async.eachSeries(this.plugins, (plugin : SearchLyrics, callback) => {
+            console.log('buscando en: ' + plugin.name);
+            
             plugin.search(normalizedTitle, artist, (err, result) => {
+                            
                 if (!err) {
+                    
                     from.lyric = result.lyric;
                     from.sourceName = plugin.name;
                     from.sourceUrl = result.url;
+                    console.log("Encontro: " + from.lyric);
                 }
                 callback(null, from)
             })
